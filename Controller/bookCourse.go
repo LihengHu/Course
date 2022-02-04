4a588264984d78574ec14b933496d48409075109
xyh1756 <1756221497@qq.com>
2022-02-05 01:18:47 +0800

    抢课模块中，验证课程是否有余量以及学生是否选过这门课采用lua脚本更新数据

--- Controller/bookCourse.go
+++ Controller/bookCourse.go
@@ -5,14 +5,23 @@ import (
 	"Course/global"
 	"Course/types"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
-	"strconv"
+	"gorm.io/gorm"
 )
 
 func BookCourse(c *gin.Context) {
-	//TODO: 考虑高并发情况
 	StudentID := c.PostForm("StudentID")
 	CourseID := c.PostForm("CourseID")
+	//清除脏数据时取消注释
+	//err := global.RDB.Del(global.CTX, "schedules").Err()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//err = global.RDB.Del(global.CTX, "courses").Err()
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	// 检验学生是否存在
 	count, err := global.RDB.SCard(global.CTX, "members").Result()
@@ -20,12 +29,20 @@ func BookCourse(c *gin.Context) {
 		panic(err)
 	}
 	if count == 0 {
-		var redisMembers []string
-		global.DB.Table("members").Select("User_ID").Where("User_Type = ?", 2).Find(&redisMembers)
-		err := global.RDB.SAdd(global.CTX, "members", redisMembers).Err()
+		global.MutexMembers.Lock()
+		count, err := global.RDB.SCard(global.CTX, "members").Result()
 		if err != nil {
 			panic(err)
 		}
+		if count == 0 {
+			var redisMembers []string
+			global.DB.Table("members").Select("User_ID").Where("User_Type = ?", 2).Find(&redisMembers)
+			err := global.RDB.SAdd(global.CTX, "members", redisMembers).Err()
+			if err != nil {
+				panic(err)
+			}
+		}
+		global.MutexMembers.Unlock()
 	}
 	isStudent, err := global.RDB.SIsMember(global.CTX, "members", StudentID).Result()
 	if err != nil {
@@ -44,16 +61,24 @@ func BookCourse(c *gin.Context) {
 		panic(err)
 	}
 	if count == 0 {
-		var courses []*Form.RedisCourse
-		global.DB.Table("courses").Select("Course_ID,Course_Cap").Find(&courses)
-		redisCourses := make(map[string]interface{})
-		for _, course := range courses {
-			redisCourses[course.Course_ID] = course.Course_Cap
-		}
-		err := global.RDB.HMSet(global.CTX, "courses", redisCourses).Err()
+		global.MutexCourses.Lock()
+		count, err = global.RDB.HLen(global.CTX, "courses").Result()
 		if err != nil {
 			panic(err)
 		}
+		if count == 0 {
+			var courses []*Form.RedisCourse
+			global.DB.Table("courses").Select("Course_ID,Course_Cap").Find(&courses)
+			redisCourses := make(map[string]interface{})
+			for _, course := range courses {
+				redisCourses[course.Course_ID] = course.Course_Cap
+			}
+			err := global.RDB.HMSet(global.CTX, "courses", redisCourses).Err()
+			if err != nil {
+				panic(err)
+			}
+		}
+		global.MutexCourses.Unlock()
 	}
 	isCourse, err := global.RDB.HExists(global.CTX, "courses", CourseID).Result()
 	if !isCourse {
@@ -65,25 +90,75 @@ func BookCourse(c *gin.Context) {
 	if err != nil {
 		panic(err)
 	}
-	courseCapStr, err := global.RDB.HGet(global.CTX, "courses", CourseID).Result()
+
+	// 刷新选课缓存
+	countSchedule, err := global.RDB.SCard(global.CTX, "schedules").Result()
 	if err != nil {
 		panic(err)
 	}
-	courseCap, _ := strconv.Atoi(courseCapStr)
-	if courseCap <= 0 {
+	if countSchedule == 0 {
+		global.MutexSchedules.Lock()
+		countSchedule, err := global.RDB.SCard(global.CTX, "schedules").Result()
+		if err != nil {
+			panic(err)
+		}
+		if countSchedule == 0 {
+			var schedules []*Form.Schedule
+			global.DB.Table("schedules").Select("Course_ID,Student_ID").Find(&schedules)
+			if len(schedules) > 0 {
+				var redisSchedules []string
+				for _, schedule := range schedules {
+					redisSchedules = append(redisSchedules, schedule.CourseID+"_"+schedule.StudentID)
+				}
+				err := global.RDB.SAdd(global.CTX, "schedules", redisSchedules).Err()
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				err := global.RDB.SAdd(global.CTX, "schedules", "nothing").Err()
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+		global.MutexSchedules.Unlock()
+	}
+
+	scheduleID := CourseID + "_" + StudentID
+	const LuaScript = `
+		local scheduleKey = KEYS[1]
+		local courseKey = KEYS[2]
+        local scheduleID = KEYS[3]
+        local courseID = KEYS[4]
+		local cap = tonumber(redis.call('HGet', courseKey, courseID))
+		if cap <= 0 then
+			return 1
+		end
+		local isBooked = redis.call('SIsMember', scheduleKey, scheduleID)
+		if isBooked ~= 0 then
+			return 2
+		end
+		redis.call('SAdd', scheduleKey, scheduleID)
+		redis.call('HIncrBy', courseKey, courseID, -1)
+		return 0
+	`
+	lua := redis.NewScript(LuaScript)
+	val, err := lua.Run(global.CTX, global.RDB, []string{"schedules", "courses", scheduleID, CourseID}).Int()
+	if err != nil {
+		panic(err)
+	}
+	if val == 1 {
 		c.JSON(200, gin.H{
 			"Code": Form.CourseNotAvailable,
 		})
 		return
-	}
-	var schedule Form.Schedule
-	global.DB.Where("Course_ID = ? and Student_ID = ?", CourseID, StudentID).First(&schedule)
-	if schedule.CourseID != "" {
+	} else if val == 2 {
 		c.JSON(200, gin.H{
 			"Code": Form.StudentHasCourse,
 		})
 		return
 	}
+	global.DB.Table("courses").Where("Course_ID = ?", CourseID).Update("Course_Cap", gorm.Expr("Course_Cap - 1"))
 	u1 := Form.Schedule{StudentID, CourseID}
 	global.DB.Create(&u1)
 	global.LOG.Info(
--- global/global.go
+++ global/global.go
@@ -5,11 +5,15 @@ import (
 	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
+	"sync"
 )
 
 var (
-	DB  *gorm.DB
-	LOG *zap.Logger
-	RDB *redis.Client
-	CTX context.Context
+	DB             *gorm.DB
+	LOG            *zap.Logger
+	RDB            *redis.Client
+	CTX            context.Context
+	MutexMembers   sync.Mutex
+	MutexCourses   sync.Mutex
+	MutexSchedules sync.Mutex
 )
